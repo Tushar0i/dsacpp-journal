1e687aa88e76ec0a346e28b75445f7ee4cc7caec
Tushar0i <tushar2006thakur@gmail.com>
2025-05-07 19:55:36 +0530

    Adding solution

--- Cpp-Fundamentals/q7.cpp
+++ Cpp-Fundamentals/q7.cpp
@@ -1,10 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 // converting a decimal number to a binary number
 
 
-int main(){
+void convtToBinr(int num)
+{
 
+    int dig, pow = 1, binary = 0 ;
+
+    do
+    {
+        dig = num % 2;
+        num /= 2;
+        binary += (dig * pow);
+
+        pow *= 10;
+
+    } while (num > 0);
+
+    cout << binary<< endl;
+}
+
+int main()
+{
+
+    convtToBinr(10);
 
     return 0;
 }
